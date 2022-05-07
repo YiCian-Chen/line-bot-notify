268fafecd66c516381460b34fdb9284f975a5b88
Wu-shin <henry112pmm4@gmail.com>
2022-05-07 12:01:36 +0800

    create ai_db & csie_db

--- database/csie_db.py
+++ database/csie_db.py
@@ -7,7 +7,7 @@ header = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "zh-TW,zh;q=0.9",
-    "Host": "enroll.tku.edu.tw",  # 目標網站
+    "Host": "www.csie.tku.edu.tw",  # 目標網站
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
     "Sec-Fetch-Site": "none",
@@ -15,9 +15,10 @@ header = {
     "User-Agent": user_agent.random,  # 使用者代理
     "Referer": "https://www.google.com/"  # 參照位址
 }
-request = requests.get('http://enroll.tku.edu.tw/', headers=header)
+request = requests.get('http://www.csie.tku.edu.tw/Front/about/news/News.aspx?id=26J0iyD8bhA=', headers=header)
 html = BeautifulSoup(request.text, 'html.parser')
-tr = html.find_all(class_='odd')
+tr = html.find_all('tr')
+tr.pop(0)
 
 
 # Update connection string information
@@ -39,16 +40,19 @@ cursor = conn.cursor()
 
 
 for i in tr:
-    left = i.find_all(align='left')
-    center = i.find_all(align='center')
-    Event_name = left[0].text
-    organizer = left[1].text
-    url = 'http://enroll.tku.edu.tw/' + left[0].a['href']
-    person = center[2].text
-    cursor.execute("INSERT INTO enroll (Event_name, organizer,url) VALUES (%s,%s,%s);", (Event_name,organizer,url))
+    td = i.find_all('td')
+    category = td[0].text
+    date = td[1].text
+    title = td[2].a.text
+    url = td[2].a['href']
+    print(category)
+    print(date)
+    print(title)
+    print(url)
+    cursor.execute("INSERT INTO csie (category,date,title,url) VALUES (%s,%s,%s,%s);", (category,date,title,url))
 print("insert seccess")
 
-cursor.execute("SELECT * FROM enroll;")
+cursor.execute("SELECT * FROM csie;")
 rows = cursor.fetchall()
 print(rows)
 
